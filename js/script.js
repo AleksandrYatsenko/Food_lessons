b4fbd8ddfa50aca380da297fc4ffddc23d0e46ba
Alex <yatsikxvii@gmail.com>
2022-12-02 13:01:55 +0200

    add script for sending form with JSON

--- js/script.js
+++ js/script.js
@@ -203,6 +203,41 @@ document.addEventListener('DOMContentLoaded', () => {
     postData(item);
   });
 
+  // function postData (form) {
+  //   form.addEventListener('submit', (e) => {
+  //     e.preventDefault();
+
+  //     const statusMessage = document.createElement('div');
+  //     statusMessage.classList.add('status');
+  //     statusMessage.textContent = message.loading;
+  //     form.append(statusMessage);
+
+  //     const request = new XMLHttpRequest();
+  //     request.open('POST', 'server.php');
+
+  //     const formData = new FormData(form);
+
+  //     request.send(formData);
+  //     request.addEventListener('load', () => {
+  //       if(request.status === 200) {
+  //         console.log(request.response);
+  //         statusMessage.textContent = message.success;
+  //         form.reset();
+  //         setTimeout(() => {
+  //           statusMessage.remove();
+  //         }, 3000);
+  //       } else {
+  //         statusMessage.textContent = message.failure;
+  //       }
+  //     });
+  //   })
+  // }
+
+  // with JSON -------------
+
+  
+
+
   function postData (form) {
     form.addEventListener('submit', (e) => {
       e.preventDefault();
@@ -214,10 +249,17 @@ document.addEventListener('DOMContentLoaded', () => {
 
       const request = new XMLHttpRequest();
       request.open('POST', 'server.php');
-
+      request.setRequestHeader('Content-type', 'application/json');
       const formData = new FormData(form);
 
-      request.send(formData);
+      const object = {};
+      formData.forEach((value, key) => {
+        object[key] = value;
+      });
+
+      const json = JSON.stringify(object);
+      
+      request.send(json);
       request.addEventListener('load', () => {
         if(request.status === 200) {
           console.log(request.response);
