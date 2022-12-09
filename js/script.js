48204c4b9acd4edb9dc92b3c71c63133eb5e9278
Alex <yatsikxvii@gmail.com>
2022-12-09 13:06:45 +0200

    add fetch api

--- js/script.js
+++ js/script.js
@@ -233,10 +233,7 @@ document.addEventListener('DOMContentLoaded', () => {
   //   })
   // }
 
-  // with JSON -------------
-
-  
-
+  // ---------- with JSON ------------- and fetch----
 
   function postData (form) {
     form.addEventListener('submit', (e) => {
@@ -251,34 +248,59 @@ document.addEventListener('DOMContentLoaded', () => {
       // form.append(statusMessage);
       form.insertAdjacentElement('afterend', statusMessage);
 
-      const request = new XMLHttpRequest();
-      request.open('POST', 'server.php');
-      request.setRequestHeader('Content-type', 'application/json');
+      // не надо для fetch:
+      // const request = new XMLHttpRequest();
+      // request.open('POST', 'server.php');
+      // request.setRequestHeader('Content-type', 'application/json');
+
       const formData = new FormData(form);
 
-      const object = {};
-      formData.forEach((value, key) => {
-        object[key] = value;
-      });
-
-      const json = JSON.stringify(object);
+      // не надо для fetch:
+      // const object = {};
+      // formData.forEach((value, key) => {
+      //   object[key] = value;
+      // });
+      // const json = JSON.stringify(object);
       
-      request.send(json);
-      request.addEventListener('load', () => {
-        if(request.status === 200) {
-          console.log(request.response);
-          // statusMessage.textContent = message.success;
-          showThanksModal(message.success);
-          form.reset();
-          // setTimeout(() => {
-          //   statusMessage.remove();
-          // }, 3000);
-          statusMessage.remove();
-        } else {
-          // statusMessage.textContent = message.failure;
-          showThanksModal(message.failure);
-        }
-      });
+      // не надо для fetch:
+      // request.send(json);
+
+      fetch('server.php', {
+        method: "POST",
+        // headers: {
+        //   'Content-type': 'application/json'
+        // },
+        body: formData
+      })
+      .then(data => data.text())
+      .then(data => {
+        console.log(data);
+        showThanksModal(message.success);
+        statusMessage.remove();
+      })
+      .catch(() => {
+        showThanksModal(message.failure);
+      })
+      .finally(() => {
+        form.reset();
+      })
+
+      // не надо для fetch:
+      // request.addEventListener('load', () => {
+      //   if(request.status === 200) {
+      //     console.log(request.response);
+      //     // statusMessage.textContent = message.success;
+      //     showThanksModal(message.success);
+      //     form.reset();
+      //     // setTimeout(() => {
+      //     //   statusMessage.remove();
+      //     // }, 3000);
+      //     statusMessage.remove();
+      //   } else {
+      //     // statusMessage.textContent = message.failure;
+      //     showThanksModal(message.failure);
+      //   }
+      // });
     })
   }
   function showThanksModal (message) {
@@ -303,6 +325,7 @@ document.addEventListener('DOMContentLoaded', () => {
       closeModal();
     }, 4000);
   }
+
 
 });
 
