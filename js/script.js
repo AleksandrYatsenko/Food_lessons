764bd39254675ec17cc77f7a2f889e7c0a1ff3e3
Alex <yatsikxvii@gmail.com>
2023-01-15 14:01:14 +0200

    change method of render

--- js/script.js
+++ js/script.js
@@ -201,36 +201,36 @@ document.addEventListener('DOMContentLoaded', () => {
   };
 
   // ------------------------------- with Classes -------------
-  // getResouce('http://localhost:3000/menu')
-  //   .then(data => {
-  //     data.forEach(({img, altimg, title, descr, price}) => {
-  //       new Card(img, altimg, title, descr, price, '.menu .container').makeCard();
-  //     })
-  //   });
+  getResouce('http://localhost:3000/menu')
+    .then(data => {
+      data.forEach(({img, altimg, title, descr, price}) => {
+        new Card(img, altimg, title, descr, price, '.menu .container').makeCard();
+      })
+    });
 
   
   // --------- without Classes ----------
-  getResouce('http://localhost:3000/menu')
-    .then(data => createCard(data));
+  // getResouce('http://localhost:3000/menu')
+  //   .then(data => createCard(data));
 
-  function createCard(data) {
-    data.forEach(({img, altimg, title, descr, price}) => {
-      const element = document.createElement('div');
-      price = price * 40;
-      element.classList.add('menu__item');
-      element.innerHTML =`
-        <img src=${img} alt=${altimg}>
-        <h3 class="menu__item-subtitle">Меню "${title}"</h3>
-        <div class="menu__item-descr">${descr}</div>
-        <div class="menu__item-divider"></div>
-        <div class="menu__item-price">
-          <div class="menu__item-cost">Цена:</div>
-          <div class="menu__item-total"><span>${price}</span> грн/день</div>
-        </div>
-      `;
-      document.querySelector('.menu .container').append(element);
-    });
-  }
+  // function createCard(data) {
+  //   data.forEach(({img, altimg, title, descr, price}) => {
+  //     const element = document.createElement('div');
+  //     price = price * 40;
+  //     element.classList.add('menu__item');
+  //     element.innerHTML =`
+  //       <img src=${img} alt=${altimg}>
+  //       <h3 class="menu__item-subtitle">Меню "${title}"</h3>
+  //       <div class="menu__item-descr">${descr}</div>
+  //       <div class="menu__item-divider"></div>
+  //       <div class="menu__item-price">
+  //         <div class="menu__item-cost">Цена:</div>
+  //         <div class="menu__item-total"><span>${price}</span> грн/день</div>
+  //       </div>
+  //     `;
+  //     document.querySelector('.menu .container').append(element);
+  //   });
+  // }
   // ---------------- without Classes ------ /\ ---
 
 
